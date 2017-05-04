2cc5ea179c5701f5a3ddf2034de43ac9042bf389
Работничек <frey.dev@yandex.ru>
2017-05-05 03:54:59 +0500

    version up

--- dist/prod/sailplay-magic.js
+++ dist/prod/sailplay-magic.js
@@ -38858,46 +38858,46 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    var country, city, number;
 
-	    if (value.slice(0, 2) == '614') {
+	    if (value.slice(0, 3) == '614') {
 	      country = '61 4';
 	      city = '';
 	      number = value.slice(3);
 
 	      number = number.slice(0, 4) + '-' + number.slice(4);
-	    } else {
-
-	      switch (value.length) {
-	        case 10:
-	          // +1PPP####### -> C (PPP) ###-####
-	          country = 1;
-	          city = value.slice(0, 3);
-	          number = value.slice(3);
-	          break;
-
-	        case 11:
-	          // +CPPP####### -> CCC (PP) ###-####
-	          country = value[0];
-	          city = value.slice(1, 4);
-	          number = value.slice(4);
-	          break;
-
-	        case 12:
-	          // +CCCPP####### -> CCC (PP) ###-####
-	          country = value.slice(0, 3);
-	          city = value.slice(3, 5);
-	          number = value.slice(5);
-	          break;
-
-	        default:
-	          return tel;
-	      }
-
-	      if (country == 1) {
-	        country = "";
-	      }
-
-	      number = number.slice(0, 3) + '-' + number.slice(3);
+	      return (country + ' ' + number).trim();
 	    }
+
+	    switch (value.length) {
+	      case 10:
+	        // +1PPP####### -> C (PPP) ###-####
+	        country = 1;
+	        city = value.slice(0, 3);
+	        number = value.slice(3);
+	        break;
+
+	      case 11:
+	        // +CPPP####### -> CCC (PP) ###-####
+	        country = value[0];
+	        city = value.slice(1, 4);
+	        number = value.slice(4);
+	        break;
+
+	      case 12:
+	        // +CCCPP####### -> CCC (PP) ###-####
+	        country = value.slice(0, 3);
+	        city = value.slice(3, 5);
+	        number = value.slice(5);
+	        break;
+
+	      default:
+	        return tel;
+	    }
+
+	    if (country == 1) {
+	      country = "";
+	    }
+
+	    number = number.slice(0, 3) + '-' + number.slice(3);
 	    return (country + " (" + city + ") " + number).trim();
 	  };
 	});
