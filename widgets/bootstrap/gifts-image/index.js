12489e0331935b1f0a97536d58eecfc7806ede6e
saike <kishkoglot@gmail.com>
2018-04-17 14:42:02 +0300

    gifts coupons refactored, history pagination fixed

--- widgets/bootstrap/gifts-image/index.js
+++ widgets/bootstrap/gifts-image/index.js
@@ -19,6 +19,8 @@ const widget = {
       scope.onChange = () => {
       };
 
+      scope.purchased_gift = false;
+
       $rootScope.$on("gift:state", (e, state) => {
         if(state){
           scope.getGift(angular.copy(state));
@@ -31,6 +33,8 @@ const widget = {
         $rootScope.$apply(() => {
           scope.show_gift = false;
           scope.show_success = true;
+          scope.purchased_gift = res;
+          console.log(scope.purchased_gift);
         });
       });
 
